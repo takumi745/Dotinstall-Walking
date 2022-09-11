d43c2245af0c7423fe98dc0c884dcfe58dfe90a9
Takumi Zoza <takumi0207@icloud.com>
2022-09-12 02:26:22 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+'use strict';
+
+
+    function inViewcallback(entries, obs) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            }
+
+            entry.target.classList.add('appear');
+            obs.unobserve(entry.target);
+        });
+    }
+
+    function onScrollcallback(entries) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                header.classList.add('scrolled');
+                toTop.classList.add('scrolled');
+            } else {
+                header.classList.remove('scrolled');
+                toTop.classList.remove('scrolled');
+            }
+        });
+    }
+
+    const header = document.querySelector('header');
+    const toTop = document.getElementById('to_top');
+
+{
+    // Intersection Observer API
+    const inViewobserver = new IntersectionObserver(inViewcallback, {
+        threshold: 0.2,
+    });
+
+    document.querySelectorAll('.animate').forEach(el => {
+        inViewobserver.observe(el);
+    });
+
+    const onScrollobserver = new IntersectionObserver(onScrollcallback);
+    onScrollobserver.observe(document.getElementById('target'));
+
+    toTop.addEventListener('click', e => {
+        e.preventDefault();
+
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    });
+}
